00dccc2670091bf7a3e5b2106c355275567e2e9c
seiobata <s.bautista18@yahoo.com>
2025-02-19 09:21:56 -0600

    add main.sh, main.py, src/, textnode, and gitignore

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+def main():
+    new_node = TextNode("This is dummy text", TextType.NORMAL, "https://github.com/seiobata/")
+    print(new_node)
+
+
+if __name__ == "__main__":
+    main()

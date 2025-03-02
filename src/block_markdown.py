08d1bcabae7d28280df1fe1de8981075a42c9533
seiobata <s.bautista18@yahoo.com>
2025-03-02 10:20:14 -0600

    add markdown_to_html_node and helper functions

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from textnode import TextNode, TextType, text_node_to_html_node
+from inline_markdown import text_to_textnodes
+from htmlnode import ParentNode
 
 
 class BlockType(Enum):
@@ -9,6 +12,7 @@ class BlockType(Enum):
     UNLIST = "unordered list"
     ORLIST = "ordered list"
 
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
     new_blocks = []
@@ -17,6 +21,7 @@ def markdown_to_blocks(markdown):
         if new_block.strip():
             new_blocks.append(new_block)
     return new_blocks
+
 
 def block_to_blocktype(block):
     lines = block.split("\n")
@@ -49,3 +54,95 @@ def block_to_blocktype(block):
             count += 1
         return BlockType.ORLIST
     return BlockType.PARAGRAPH
+
+# This is the main function of this file
+def markdown_to_html_node(markdown):
+    blocks = markdown_to_blocks(markdown)
+    child_nodes = []
+    for block in blocks:
+        block_type = block_to_blocktype(block)
+        html_node = block_to_html_node(block, block_type)
+        child_nodes.append(html_node)
+    return ParentNode("div", child_nodes, None)
+
+
+def block_to_html_node(block, block_type):
+    match block_type:
+        case BlockType.PARAGRAPH:
+            return make_paragraph_node(block)
+        case BlockType.HEADING:
+            return make_heading_node(block)
+        case BlockType.CODE:
+            return make_code_node(block)
+        case BlockType.QUOTE:
+            return make_quote_node(block)
+        case BlockType.UNLIST:
+            return make_unlist_node(block)
+        case BlockType.ORLIST:
+            return make_orlist_node(block)
+        case _:
+            raise ValueError("invalid block type")
+
+# converts inline markdown to list of LeafNodes
+def text_to_children(text):
+    text_nodes = text_to_textnodes(text)
+    child_nodes = []
+    for text_node in text_nodes:
+        html_node = text_node_to_html_node(text_node)
+        child_nodes.append(html_node)
+    return child_nodes
+
+
+def make_paragraph_node(block):
+    lines = block.split("\n")
+    new_block = " ".join(lines)
+    return ParentNode("p", text_to_children(new_block))
+
+
+def make_heading_node(block):
+    count = 0
+    for char in block:
+        if char == "#":
+            count += 1
+        else:
+            break
+    new_block = block[count + 1:]
+    return ParentNode(f"h{count}", text_to_children(new_block))
+
+
+def make_code_node(block):
+    new_block = block[4:-3]
+    code_node = text_node_to_html_node(TextNode(new_block, TextType.NORMAL))
+    code_node = ParentNode("code", [code_node])
+    return ParentNode("pre", [code_node])
+
+
+def make_quote_node(block):
+    lines = block.split("\n")
+    new_lines = []
+    for line in lines:
+        new_lines.append(line.lstrip("> "))
+    new_block = " ".join(new_lines)
+    return ParentNode("blockquote", text_to_children(new_block))
+
+
+def make_unlist_node(block):
+    lines = block.split("\n")
+    new_block = []
+    for line in lines:
+        new_lines = text_to_children(line[2:])
+        new_block.append(ParentNode("li", new_lines))
+    return ParentNode("ul", new_block)
+
+
+def make_orlist_node(block):
+    lines = block.split("\n")
+    new_block = []
+    for line in lines:
+        list_num = line.find(". ")
+        if list_num != -1:
+            new_lines = text_to_children(line[list_num + 2:])
+            new_block.append(ParentNode("li", new_lines))
+        else:
+            raise ValueError("invalid ordered list")
+    return ParentNode("ol", new_block)
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -4,6 +4,7 @@ from block_markdown import (
     BlockType,
     markdown_to_blocks,
     block_to_blocktype,
+    markdown_to_html_node,
 )
 
 
@@ -69,6 +70,87 @@ This is the same paragraph on a new line
 
         text5 = "- something unordered\n- another line"
         self.assertEqual(block_to_blocktype(text5), BlockType.UNLIST)
+
+# Testing markdown to html function
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>"
+        )
+
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>"
+        )
+
+
+    def test_heading(self):
+        md = """
+## this is an h2 heading
+
+some paragraph in the middle
+
+# another h1 heading
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h2>this is an h2 heading</h2><p>some paragraph in the middle</p><h1>another h1 heading</h1></div>"
+        )
+
+
+    def test_quote(self):
+        md = """
+> some multi line
+> blockquote with
+
+a paragraph too
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>some multi line blockquote with</blockquote><p>a paragraph too</p></div>"
+        )
+
+
+    def test_multi_list(self):
+        md = """
+1. one and
+2. two and
+3. buckle my shoe
+
+- roses are red
+- violets are blue
+- this is item 3
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>one and</li><li>two and</li><li>buckle my shoe</li></ol><ul><li>roses are red</li><li>violets are blue</li><li>this is item 3</li></ul></div>"
+        )
 
 
 if __name__ == "__main__":

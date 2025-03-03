ccbac7d3e27ab32c3b8010fa9281a4030c676712
seiobata <s.bautista18@yahoo.com>
2025-03-03 16:24:29 -0600

    add directory copy function to main

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
-from textnode import TextNode, TextType
+import os, shutil
+
 
 def main():
-    new_node = TextNode("This is dummy text", TextType.NORMAL, "https://github.com/seiobata/")
-    print(new_node)
+    print("Deleting public directory...")
+    copy_source_to_dest("static", "public")
+
+
+def copy_source_to_dest(source, dest):
+    if os.path.exists(dest):
+        shutil.rmtree(dest)
+    os.mkdir(dest)
+    paths = os.listdir(source)
+    for path in paths:
+        source_path = os.path.join(source, path)
+        dest_path = os.path.join(dest, path)
+        if os.path.isfile(source_path):
+            shutil.copy(source_path, dest_path)
+            print(f"Copying {source_path} to {dest_path}")
+        else:
+            copy_source_to_dest(source_path, dest_path)
 
 
 if __name__ == "__main__":
